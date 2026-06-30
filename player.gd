187aa89f4e730ea0d9a218c5bf510e3d87db4767
yrieth <misha525573@gmail.com>
2026-06-30 19:27:55 +0200

    Life and mana pools

--- player.gd
+++ player.gd
@@ -2,8 +2,23 @@ extends CharacterBody2D
 
 const BASE_VELOCITY: int = 1000
 const BASE_LIGHT_RADIUS: int = 100
+const BASE_LIFE: int = 100
+const BASE_MANA: int = 100
+
+var max_life: int
+var life: int
+var max_mana: int
+var mana: int
+
 
 func _ready() -> void:
+	life = 100
+	mana = 100
+	calculate_max_life()
+	update_life(max_life, false)
+	calculate_max_mana()
+	update_mana(max_mana, false)
+	mana = max_mana
 	visible = false
 	position = Vector2($"../Map".MAP_SIZE*8,$"../Map".MAP_SIZE*8)
 
@@ -26,3 +41,44 @@ func calculate_velocity() -> void:
 
 func _on_tile_map_gaea_renderer_render_finished() -> void:
 	visible = true
+	
+func calculate_max_life() -> void:
+	max_life = BASE_LIFE
+	
+	$"../UI/LifeRect/LifePool".max_value = max_life
+	$"../UI/LifeRect/Label".text = str(life)+"/"+str(max_life)
+
+func update_life(amount: int, additive: bool = true) -> void:
+	if additive:
+		life = mini(max_life, life+amount)
+	else :
+		life = mini(max_life, amount)
+	$"../UI/LifeRect/LifePool".value = life
+	$"../UI/LifeRect/Label".text = str(life)+"/"+str(max_life)
+	
+func calculate_max_mana() -> void:
+	max_mana = BASE_MANA
+	
+	$"../UI/ManaRect/ManaPool".max_value = max_mana
+	$"../UI/ManaRect/Label".text = str(mana)+"/"+str(max_mana)
+	
+
+func update_mana(amount: int, additive: bool = true) -> void:
+	if additive:
+		mana = mini(max_mana, mana+amount)
+	else :
+		mana = mini(max_mana, amount)
+	$"../UI/ManaRect/ManaPool".value = mana
+	$"../UI/ManaRect/Label".text = str(mana)+"/"+str(max_mana)
+
+func _on_life_hover_box_mouse_entered() -> void:
+	$"../UI/LifeRect/Label".visible = true
+
+func _on_life_hover_box_mouse_exited() -> void:
+	$"../UI/LifeRect/Label".visible = false
+
+func _on_mana_hover_box_mouse_entered() -> void:
+	$"../UI/ManaRect/Label".visible = true
+
+func _on_mana_hover_box_mouse_exited() -> void:
+	$"../UI/ManaRect/Label".visible = false
